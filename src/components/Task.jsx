67533d2142227637985daeb94713abe125bff80d
21MoududAhammed <21moudud@gmail.com>
2024-01-29 09:01:54 +0600

    now the task manager is done with useReducer

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,12 +2,48 @@ import { useState } from "react";
 
 export default function Task({ task, dispatch }) {
   const [isEdit, setIsEdit] = useState(true);
+  const [value, setValue] = useState(task.text);
   return (
     <li>
-      <input type="checkbox" />
-      {isEdit ? task.text : <input type="text" />}{" "}
-      <button>{isEdit ? "Edit" : "Save"}</button>
-      <button>Delete</button>
+      <input
+        type="checkbox"
+        defaultChecked={task.done}
+        onClick={() => {
+          dispatch({
+            type: "edited",
+            task: { ...task, done: !task.done },
+          });
+        }}
+      />
+      {isEdit ? (
+        task.text
+      ) : (
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      )}{" "}
+      <button
+        onClick={() => {
+          setIsEdit(!isEdit);
+          dispatch({
+            type: "edited",
+            task: {
+              ...task,
+              text: value,
+            },
+          });
+        }}
+      >
+        {isEdit ? "Edit" : "Save"}
+      </button>
+      <button onClick={()=>{
+        dispatch({
+          type: 'deleted',
+          id: task.id
+        })
+      }}>Delete</button>
     </li>
   );
 }
--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -3,6 +3,12 @@ const taskReducer = (tasks, action) => {
     case "added": {
       return [...tasks, { ...action.task }];
     }
+    case "edited": {
+      return tasks.map((t) => (t.id === action.task.id ? action.task : t));
+    }
+    case "deleted": {
+      return tasks.filter((task) => task.id !== action.id);
+    }
     default: {
       throw Error(`Unknown type ${action.type}`);
     }
